f98d58abdbbfcfe4afe9ee23e6ee60d52cd8af92
Jongkyun <hjk326333@gmail.com>
2025-04-21 15:43:16 +0900

    chap04: 단방향 연관관계 확인, FetchType Eager와 Lazy의 차이 확인

--- src/main/java/com/study/jpa/chap04/entity/Department.java
+++ src/main/java/com/study/jpa/chap04/entity/Department.java
@@ -3,6 +3,8 @@ package com.study.jpa.chap04.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @Setter
 @Getter
 @ToString
@@ -22,5 +24,11 @@ public class Department {
 
     @Column(name = "dept_name", nullable = false)
     private String name;
+
+    // 양방향 맵핑에서는 실제 테이블에 list가 세팅되지 않습니다.
+    // 엔터티 안에서만 사용하는 가상의 컬럼입니다.
+    // 상대방 엔터티의 갱신에 관여할 수 없기 때문에 단순히 읽기 전용(조회)으로만 사용하는 것을 권장
+    @OneToMany(mappedBy = "department") // 연관관계 엔터티의 필드명을 작성
+    private List<Employee> employees;
 
 }
--- src/test/java/com/study/jpa/chap04/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/study/jpa/chap04/repository/DepartmentRepositoryTest.java
@@ -1,7 +1,24 @@
 package com.study.jpa.chap04.repository;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Transactional
 class DepartmentRepositoryTest {
+
+//    @Test
+//    @DisplayName("")
+//    void () {
+//        // given
+//
+//        // when
+//
+//        // then
+//    }
 
 }
